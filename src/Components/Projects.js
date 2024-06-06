19372014de6a7d66ecf7c52cfb6bca81569d3200
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-06-06 15:26:25 +0530

    complete projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,11 +12,24 @@ export default function Projects() {
     <div className='projects-container'>
 
       <section className="project">
+        <h3>Task Manager</h3>
         <a href='https://github.com/abdulshaikh55/task_manager'>
-          <h3>Task Manager</h3>
           <img src={task_manager} alt='Screenshot of Task Manager TUI' />
-          <p className='description'>This command line application is built with Rust and features a Terminal User Inteface (TUI). It empowers users to efficiently manager their tasks in a fun and interactive way.</p>
         </a>
+        <p className='description'>This command line application is built with Rust and features a Terminal User Interface (TUI). It empowers users to efficiently manager their tasks in a fun and interactive way.</p>
+
+        <div className='info'>
+          The goal of this project was to learn a novel idea (Terminal User Interface) in a set amount of time. Not only did I learn how to make a TUI, but I also learned:
+          <li>how to program layout and controls using ratatui</li>
+          <li>how to connect the application to the terminal using crossterm</li>
+          <li>how to efficiently develop a software</li>
+          <li>how to set goals</li>
+          <li>how to manage a project</li>
+          <li>how to research tech from their documentations</li>
+          <li>how to document a project</li>
+          and most importantly, <span>I learned how to learn.</span>
+        </div>
+
         <ul>
           <li>Rust</li>
           <li>crossterm</li>
@@ -29,11 +42,22 @@ export default function Projects() {
       </section>
 
       <section className="project">
+        <h3>Personal Finance Management Dashboard</h3>
         <a href='https://github.com/abdulshaikh55/Personal-Finance-Management'>
-          <h3>Personal Finance Management Dashboard</h3>
           <img src={personal_finance} alt='Screenshot of PFMD Webapp' />
-          <p className='description'>This project contributes to the development of innovative personal finance tools, potentially impacting personal financial literacy and fostering responsible decisions among users.</p>
         </a>
+        <p className='description'>This project contributes to the development of innovative personal finance tools, potentially impacting personal financial literacy and fostering responsible decisions among users.</p>
+
+        <div className='info'>
+          The goal of this project was to learn the Web Development in a collaborative environment. Not only did I learn how to apply the Software Engineering Cycle, but I also learned:
+          <li>how to work collaboratively with a team</li>
+          <li>how to gather and analyze requirements</li>
+          <li>how to design and develop a viable product</li>
+          <li>how to efficiently manage a team project</li>
+          <li>how to research and implement new technologies</li>
+          and I also <span>learned how to work with others.</span>
+        </div>
+
         <ul>
           <li>Collaboration</li>
           <li>Requirement Analysis</li>
